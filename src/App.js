b90ac71f7c7866acbb0e4b983cb23bd0bf673366
Pedro Diaz <pjdux@ilianas-MacBook-Pro.local>
2020-12-11 00:25:22 -0500

    stable

--- src/App.js
+++ src/App.js
@@ -1,19 +1,84 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 const App = () => {
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [formValues, setFormValues] = useState("");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setFormValues(formData);
+  };
+  const handleReset = () => {
+    setFormData({ email: "", name: "", subject: "", message: "" });
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const { email, name, subject, message } = formData;
+
+    axios.post("/sendtome", {
+      //make an object to be handled from req.body on the backend.
+      email,
+      name,
+      subject,
+      //change the name to represent text on the backend.
+      text: message,
+    });
+
+    console.log(formData);
+    handleReset();
+  };
 
   return (
     <div className="App-header">
-      <form style={{ width: "300px" }} className="App-header">
-        <label>Name</label>
-        <input type="text" name="name" />
-        <label>Email</label>
-        <input type="text" name="email" />
-        <label>Message</label>
-        <textarea type="text" name="message" rows="8" />
-        <input type="submit" />
+      <form
+        onSubmit={onFormSubmit}
+        style={{ width: "300px" }}
+        className="App-header"
+      >
+        <label htmlFor="email">Email</label>
+        <input
+          value={formData.email}
+          onChange={handleChange}
+          type="text"
+          name="email"
+          id="email"
+        />
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          value={formData.name}
+          onChange={handleChange}
+          type="text"
+          id="name"
+          name="name"
+        />
+        <label htmlFor="email">Subject</label>
+        <input
+          value={formData.subject}
+          onChange={handleChange}
+          type="text"
+          name="subject"
+          id="subject"
+        />
+        <label htmlFor="message">Message</label>
+        <textarea
+          value={formData.message}
+          onChange={handleChange}
+          type="text"
+          name="message"
+          rows="8"
+        />
+        <input id="submit-button" type="submit" />
       </form>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import {
+  render,
+  screen,
+  fireEvent,
+  rendeeIntoDocument,
+  cleanup,
+  getByLabelText,
+} from "@testing-library/react";
+import App from "./App";
+import { renderIntoDocument } from "react-dom/test-utils";
+afterEach(cleanup);
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Submits and clears data", () => {
+  const onFormSubmit = jest.fn();
+  const { getByLabelText, getByText } = renderIntoDocument(
+    <App onSubmit={onFormSubmit} />
+  );
+  getByLabelText(/name/i).value = "Pedro";
+  getByLabelText(/email/i).value = "pjdazeux@gmail.com";
+  getByText(/submit/i).click();
+  expect(onFormSubmit).toHaveBeenCalledTimes(1);
+
 });
